05a3d104770d3374f174cb4dbcbf8e0c3cefd3ca
NeoGendaijin <45292684+NeoGendaijin@users.noreply.github.com>
2025-08-29 13:15:55 +0900

    Fix code issues for iteration 5

--- src/evaluate.py
+++ src/evaluate.py
@@ -14,7 +14,7 @@ from .train import LinearRidgeReadout, SRCReservoir
 from .preprocess import add_gaussian_noise, translate_zeropad, to_onehot
 from .train import collect_features_encoder_reservoir
 
-IMAGE_DIR = os.path.join('.research', 'iteration4', 'images')
+IMAGE_DIR = os.path.join('.research', 'iteration5', 'images')
 os.makedirs(IMAGE_DIR, exist_ok=True)
 
 # Improve PDF quality
